2f50c24ae55f2e9b753dc18e5684551b3d8b53d9
Matthew Trip <matthewtrip2@gmail.com>
2020-04-23 10:53:59 +0100

    Doc update

--- Rid/Enums/Infraction.cs
+++ Rid/Enums/Infraction.cs
@@ -2,9 +2,24 @@
 {
     public enum Infraction
     {
+        /// <summary>
+        /// Represents a ban infraction.
+        /// </summary>
         Ban,
+        
+        /// <summary>
+        /// Represents a kick infraction.
+        /// </summary>
         Kick,
+        
+        /// <summary>
+        /// Represents a mute infraction.
+        /// </summary>
         Mute,
+        
+        /// <summary>
+        /// Represents a warning infraction.
+        /// </summary>
         Warn
     }
 }
